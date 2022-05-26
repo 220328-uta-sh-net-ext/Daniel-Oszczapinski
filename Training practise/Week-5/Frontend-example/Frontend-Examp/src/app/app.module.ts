ef8054aa7a452f04b50f29fb6f477837421456dd
Daniel Oszczapinski <dannyoszczapinski@gmail.com>
2022-05-26 14:09:25 -0500

    Added frontend components and deleted others

--- Training practise/Week-5/Frontend-example/Frontend-Examp/src/app/app.module.ts
+++ Training practise/Week-5/Frontend-example/Frontend-Examp/src/app/app.module.ts
@@ -3,28 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
-//import { DanielInfoComponent } from './daniel-info/daniel-info.component';
-//import { EdithComponent } from './edith/edith.component';
-//import { VladimirComponent } from './vladimir/vladimir.component';
-//import { BricesonComponent } from './briceson/briceson.component';
 import { FormsModule } from '@angular/forms';
 import { PersonComponent } from './person/person.component';
 import { RosterComponent } from './roster/roster.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SquarePipe } from './square.pipe';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    //DanielInfoComponent,
-    //EdithComponent,
-    //VladimirComponent,
-   //BricesonComponent,
     PersonComponent,
-    RosterComponent
+    RosterComponent,
+    NavbarComponent,
+    SquarePipe
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Training practise/Week-5/Frontend-example/Frontend-Examp/src/app/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  navigateLogin(){
+    this.router.navigate(['login'])
+  }
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+}
